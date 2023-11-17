84895df9feed4cfb003d58e6b853fb7b8d37dfc6
Fabricio Canepa <fabri.caspel@gmail.com>
2023-11-17 16:44:58 -0300

    termino diseño card y modal (falta agregar fuente y ajustar tamaños)

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import MainView from "./views/MainView";
+
+import "./index.css";
+
 const Router = () => {
-
   return (
-    <>
-      
-    </>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainView />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default Router
+export default Router;
---
+++ src/views/MainView.jsx
@@ -0,0 +1,11 @@
+import CardModal from "../components/Main/Menu/CardModal.jsx";
+
+const MainView = () => {
+  return (
+    <section className="sec-main">
+      <h1 className="text-center mb-5">Cuál es tu comida para esta noche?</h1>
+      <CardModal />
+    </section>
+  );
+};
+export default MainView;
