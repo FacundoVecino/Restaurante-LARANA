bf59ffbb7290334ac702cdcc631502ebe48d3b35
Alexis Mauricio Gunsett <mau_gunsett@hotmail.com>
2023-12-02 15:50:28 -0300

    soluciono problema de nav

--- src/Router.jsx
+++ src/Router.jsx
@@ -11,41 +11,32 @@ import Profile from "./components/UserSupport/Profile";
 import Support from "./components/UserSupport/Support";
 import Navbar from "./components/Common/Navbar";
 import Footer from "./components/Common/Footer";
-import Error404 from "./views/Error404"
+import Error404 from "./views/Error404";
 
 const Router = () => {
   const { isLoggedIn } = useSession();
   return (
     <BrowserRouter>
-     <Navbar/>
+      <Navbar/>
       <main>
         <Routes>
           <Route path="/access" element={<AccessPanelView />}>
             <Route index element={<LoginView />} />
             <Route path="register" element={<RegisterView />} />
           </Route>
-          
           <Route
             path="/user"
-            element={
-              isLoggedIn ? (
-                <UserView />
-              ) : (
-                <Navigate to="/access" />
-              )
-            }
+            element={isLoggedIn ? <UserView /> : <Navigate to="/access" />}
           >
-            <Route index element={<Profile/>} />
-            <Route path="support" element={<Support/>} />
-          <Route path="*" element={<Error404/>}/>
+            <Route index element={<Profile />} />
+            <Route path="support" element={<Support />} />
+            <Route path="*" element={<Error404 />} />
           </Route>
         </Routes>
       </main>
       <Footer />
-
     </BrowserRouter>
   );
 };
 
 export default Router;
-
--- src/components/Common/Navbar.jsx
+++ src/components/Common/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <ul>
-        <li>
+        <li className='custom-li'>
           <NavLink className={`nav-link ${({ isActive }) =>
                     isActive ? "active" : ""}`}
                   aria-current="page"
@@ -15,19 +15,19 @@ const Navbar = () => {
             <FaHome />
           </NavLink>
         </li>
-        <li>
+        <li className='custom-li'>
           <NavLink to="/cart">
             <FaShoppingCart />
           </NavLink>
         </li>
-        <li>
+        <li className='custom-li'>
           <NavLink className={`nav-link ${({ isActive }) =>
                     isActive ? "active" : ""}`}
                   to="/add">
           <FaPlus />
           </NavLink>
         </li>
-        <li>
+        <li className='custom-li'>
           <NavLink   className={`nav-link ${({ isActive }) =>
                     isActive ? "active" : ""}`}
                   to="/user">
