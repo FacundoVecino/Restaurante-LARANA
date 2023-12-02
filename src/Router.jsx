2e5295e3828d0da8acbf9f20830a567428480486
Enzo Iñigo <enzoinigo96@gmail.com>
2023-12-02 16:26:57 -0300

    agrego mensaje de carrito vacio

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,21 +1,36 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { useSession } from "./stores/useSession";
+import { useState } from "react";
+
 import LoginView from "./views/LoginView";
 import RegisterView from "./views/RegisterView";
-import { useSession } from "./stores/useSession";
-import Profile from "./components/UserSupport/Profile";
 import UserView from "./views/UserView";
+import ErrorView from "./views/ErrorView";
+
+import Profile from "./components/UserSupport/Profile";
 import Support from "./components/UserSupport/Support";
+
 import Navbar from "./components/Common/Navbar";
 import Footer from "./components/Common/Footer";
-import Error404 from "./views/Error404"
 
 const Router = () => {
+  const [allProducts, setAllProducts] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [countProducts, setCountProducts] = useState(0);
   const { isLoggedIn } = useSession();
   return (
     <BrowserRouter>
-     <Navbar/>
+      <Navbar
+        allProducts={allProducts}
+        setAllProducts={setAllProducts}
+        total={total}
+        setTotal={setTotal}
+        countProducts={countProducts}
+        setCountProducts={setCountProducts}
+      />
       <main>
         <Routes>
+
           <Route
             path="/login"
             element={isLoggedIn ? <Navigate to="/user" /> : <LoginView />}
@@ -26,26 +41,17 @@ const Router = () => {
           />
           <Route
             path="/user"
-            element={
-              isLoggedIn ? (
-                <UserView />
-              ) : (
-                <Navigate to="/" />
-              )
-            }
-          >
-            {/* Rutas secundarias bajo /user */}
-            <Route index element={<Profile/>} />
-            <Route path="support" element={<Support/>} />
-          <Route path="*" element={<Error404/>}/>
-          </Route>
+            element={isLoggedIn ? <UserView /> : <Navigate to="/" />}
+          />
+          {/* Rutas secundarias bajo /user */}
+          <Route index element={<Profile />} />
+          <Route path="support" element={<Support />} />
+          <Route path="*" element={<ErrorView />} />
         </Routes>
       </main>
       <Footer />
-
     </BrowserRouter>
   );
 };
 
 export default Router;
-
