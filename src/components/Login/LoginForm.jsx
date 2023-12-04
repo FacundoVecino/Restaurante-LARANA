26f8d98e962ecbbf9b3533af968a8e7bb9eab152
Alexis Mauricio Gunsett <mau_gunsett@hotmail.com>
2023-12-04 16:56:55 -0300

    modifico estilo del input de la contraseña

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -75,12 +75,13 @@ const[showPwd, setShowPwd]= useState(false)
           required: true,
         }}
       />
+      <div className="d-flex ">
       <Input
         label="Contraseña"
         type={showPwd ? "text" : "password"}
         placeholder="Ingrese su contraseña"
         name="password"
-        className="my-3"
+        className="p-2 w-100"
         register={register}
         error={!!errors?.password}
         options={{
@@ -100,7 +101,7 @@ const[showPwd, setShowPwd]= useState(false)
               <path d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 00-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 016.75 12z" />
             </svg>}
           </div>
-
+          </div>
       <div className="mb-3">
         <ButtonRegisterLogin text="log in"/>
       </div>
--- src/views/LoginView.jsx
+++ src/views/LoginView.jsx
@@ -1,16 +1,12 @@
 import LoginForm from "../components/Login/LoginForm";
 
-
 const LoginView = () => {
   return (
-    <>
-      <section className="text-center">
-      <div className="imgRegister">
-        <div className="d-flex justify-content-center">
+    <section className="text-center">
+      <div className="imgRegister d-flex justify-content-center">
         <LoginForm />
-        </div>
-      </div></section>
-    </>
+      </div>
+    </section>
   );
 };
 export default LoginView;
