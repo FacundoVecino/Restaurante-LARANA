47d963038efa1d242aa30c59421b5317317c8666
FacundoVecino <facundovecino32@gmail.com>
2023-12-04 14:27:18 -0300

    Hago preparativos para recibir la lista de usuarios

--- src/components/Admin/AdminUsers/AdminUsers.jsx
+++ src/components/Admin/AdminUsers/AdminUsers.jsx
@@ -1,8 +1,32 @@
+// AdminUsers.jsx
+
+// import { useQuery } from "@tanstack/react-query";
+// import { getUserFn } from "../../../api/users";
+// import CardUser from "./CardUsers/CardUser";
+// import { useSession, useUser } from "../../../stores/useSession";
+// import { useEffect } from "react";
+
 const AdminUsers = () => {
+  // const { user } = useSession()
+  // const { setUserToEdit, userCurrent } = useUser()
+  // console.log(users)
+
+  // const { data: users } = useQuery({
+  //   queryKey: ["users"],
+  //   queryFn: getUserFn,
+  // });
+  // console.log("Users Data:", users);
+
+  // useEffect(() => {
+  //   setUserToEdit(userCurrent);
+  // }, [userCurrent, setUserToEdit]);
+
   return (
-    <>
-      <div>AdminUsers</div>
-    </>
+    <div className="products-table d-flex flex-wrap justify-content-center">
+      {/* {users.map((user) => (
+        <CardUser user={user} key={user.id} />
+      ))} */}
+    </div>
   );
 };
 
